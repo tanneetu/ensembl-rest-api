9b2259aaf2c30b42f2eb258b9d8aed5b2edd2fe7
Tanzina Ferdous <tanzina.ferdous@embl.de>
2024-12-05 15:21:06 +0100

    Add lookup_symbol function script

--- R/lookup_symbol.R
+++ R/lookup_symbol.R
@@ -25,7 +25,7 @@ query_string_optional_params <- function(optional_params) {
 
   if (length(optional_params) == 0) return("")
 
-  # Create key-value pairs for url
+  # Create key-value pairs for URL
   query_string <- paste(
     paste(names(optional_params), "=", optional_params, sep=""),
     collapse = ";"
@@ -33,47 +33,59 @@ query_string_optional_params <- function(optional_params) {
   return(query_string)
 }
 
-lookup_symbol <- function(species = NULL, symbol = NULL, expand = NULL) {
+post_lookup_symbol <- function(species = NULL, symbols = NULL) {
 
   all_params_list <- as.list(environment())
 
-  mandatory_params_name <- c("species", "symbol")
+  mandatory_params_name <- c("species")
 
   check_param <- function(param_value) {
-    is.null(param_value) || !nzchar(as.character(param_value)) || !is.character(param_value)
+
+    is.null(param_value) ||!nzchar(as.character(param_value)) ||
+      !is.character(param_value)
   }
 
-  #check mandatory parameters
-  is_missing <- lapply(mandatory_params_name, function(param) {
+  is_missing <- sapply(mandatory_params_name, function(param) {
     check_param(all_params_list[[param]])
   })
-
-  is_missing <- unlist(is_missing)
 
   if (any(is_missing)) {
     stop("Mandatory parameters are missing!")
   }
 
   mandatory_params <- all_params_list[mandatory_params_name]
-  optional_params_names <- setdiff(names(all_params_list), mandatory_params_name)
+  optional_params_names <- setdiff(names(all_params_list), c(mandatory_params_name, "symbols"))
   optional_params <- all_params_list[optional_params_names]
 
-  string_optional_params<- query_string_optional_params(optional_params)
+  string_optional_params <- query_string_optional_params(optional_params)
 
   endpoint <- "/lookup/symbol/"
   url <- build_url(endpoint, mandatory_params, string_optional_params)
 
-  print("The URL: ")
-  print(url)
+  body <- list(symbols = symbols)
 
-  req <- request(url) |> req_headers("Accept" = "application/json")
+  req <- request(url) |>
+    req_headers("Accept" = "application/json") |>
+    req_body_json(body, auto_unbox = FALSE)
+
+  #req_dry_run(req)
+
   resp <- req |> req_perform()
 
   content <- resp_body_json(resp)
 
-  result <- fromJSON(toJSON(content))
+  result <- head(fromJSON(toJSON(content)))
 
   return(result)
+}
 
+lookup_symbol <- function(species = NULL, symbols = NULL) {
+
+  if (length(species) == 0) {
+    stop("Species is missing!")}
+  else if (length(symbols) == 0) {
+    stop("Symbol is missing!")}
+  else{
+    return(post_lookup_symbol(species, symbols))}
 }
 
