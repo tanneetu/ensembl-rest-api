fd662dd9ccb34f789539c42b7a8ebe65fb589266
Tanzina Ferdous <tanzina.ferdous@embl.de>
2025-03-10 10:16:52 +0100

    Complete unit test for lookup_symbol

--- tests/testthat/test-lookup_symbol.R
+++ tests/testthat/test-lookup_symbol.R
@@ -1,28 +1,13 @@
 # Run once in the console only when updating the API response
 # Capture the API responses to be saved to the mock file
-capture_requests({
-  # Correct species and correct symbol
-  real_response <- lookup_symbol("human", "BRCA2")
-
-  # Correct species and one symbol is wrong (Partially empty list case)
-  partial_response <- lookup_symbol("human", c("BRCA2", "BR"))
-
-})
-
 # capture_requests({
+#   # Correct species and correct symbol
+#   real_response <- lookup_symbol("human", "BRCA2")
 #
-#   # Wrong species
-#   empty_response1 <- lookup_symbol("hum", c("BRCA2", "BRAF"))
+#   # Correct species and one symbol is wrong (Partially empty list case)
+#   partial_response <- lookup_symbol("human", c("BRCA2", "BR"))
 #
 # })
-#
-# capture_requests({
-#
-#   # Correct species but all symbols are wrong
-#   empty_response2 <- lookup_symbol("human", c("BRC2", "BR"))
-#
-# })
-
 
 
 # From the mock file the response will be loaded
@@ -31,6 +16,7 @@ with_mock_api({
   test_that("lookup_symbol() returns correct response", {
     response <- lookup_symbol("human", "BRCA2")
     expect_type(response, "list")
+    expect_false(length(response) == 0)
   })
 
   test_that("lookup_symbol() handles partially incorrect symbols", {
@@ -38,7 +24,7 @@ with_mock_api({
       response <- lookup_symbol("human", c("BRCA2", "BR")),
       "Check your input: Some symbols may be incorrect."
     )
-    #expect_type(response, "list")
+
     expect_true("BRCA2" %in% names(response))  # BRCA2 should exist
     expect_false("BR" %in% names(response))  # BR should be missing
 
@@ -48,7 +34,9 @@ with_mock_api({
 
 test_that("lookup_symbol() handles invalid species gracefully", {
 
-  #stub(lookup_symbol, "validate_species_name", function(species) FALSE)
+  stub(lookup_symbol, "validate_species_name", function(...) {
+    stop("Species name not found! Did you mean any of these: ...?")
+  })
 
   stub(lookup_symbol, "fetch_data_with_cache", function(...) list())
 
@@ -61,31 +49,31 @@ test_that("lookup_symbol() handles invalid species gracefully", {
 })
 
 
-# with_mock_api({
-#
-#   test_that("lookup_symbol() handles all incorrect symbols", {
-#     expect_error(
-#       lookup_symbol("human", c("BRC2", "BR")),
-#       regexp = "Check your input: Symbols may be incorrect.",
-#       fixed = TRUE
-#     )
-#   })
-#
-# })
-#
-# test_that("lookup_symbol() handles invalid species", {
-#   expect_error(
-#     lookup_symbol("hum", c("BRCA2", "BRAF")),
-#     regexp = "Species name not found!",
-#     fixed = TRUE  # Ensures exact match for the error message
-#   )
-# })
+test_that("lookup_symbol() handles all incorrect symbols", {
 
-# test_that("lookup_symbol() handles all incorrect symbols", {
-#   expect_error(
-#     lookup_symbol("human", c("BRC2", "BR")),
-#     regexp = "Check your input: Symbols may be incorrect.",
-#     fixed = TRUE
-#   )
-# })
+  stub(lookup_symbol, "validate_species_name", function(species) TRUE)
 
+  stub(lookup_symbol, "fetch_data_with_cache", function(...) list())
+
+  expect_error(
+    lookup_symbol("human", c("BRC2", "BR")),
+    regexp = "Check your input: Symbols may be incorrect.",
+    fixed = TRUE
+  )
+})
+
+
+# input validation
+test_that("lookup_symbol() returns error when species is missing", {
+  expect_error(
+    lookup_symbol("", "BRCA2"),
+    "Species is missing! Please provide a valid species name."
+  )
+})
+
+test_that("lookup_symbol() returns error when symbols are missing", {
+  expect_error(
+    lookup_symbol("human", ""),
+    "Symbol is missing! Please provide valid symbol name."
+  )
+})
